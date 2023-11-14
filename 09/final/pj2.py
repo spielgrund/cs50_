e7bf760ed6624e729312f83da610e156912d6053
spielgrund <138134622+spielgrund@users.noreply.github.com>
2023-11-14 12:55:30 +0100

    pj

---
+++ 09/final/pj2.py
@@ -0,0 +1,24 @@
+import sys
+
+def main():
+    parameters = get_parameters()
+    draw_tiles(parameters)
+    normalize()
+    show_output()
+
+def get_parameters():
+    ...
+
+
+def draw_tiles():
+    ...
+
+
+def normalize():
+    ...
+
+def show_output():
+    ...
+
+if __name__ == "__main__":
+    main()
